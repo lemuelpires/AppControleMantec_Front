b314d10b222a77907a4029e7d73aaf1661754c76
lemuelpires <lemuelpires@gmail.com>
2024-07-02 21:26:49 -0300

    correção erro pagina estoque parte 2

--- src/pages/Estoques/index.jsx
+++ src/pages/Estoques/index.jsx
@@ -3,11 +3,11 @@ import Modal from 'react-modal';
 import Select from 'react-select';
 import { EstoqueContainer, EstoqueTitle, EstoqueButton, EstoqueTable, BotaoEspacamento } from './style';
 import ModalDetalhesEstoque from '../../components/Modais/Estoque/ModalDetalhes';
-import ModalEdicaoEstoque from '../../components/Modais/Estoque/ModalEdicao'; // Importe o componente corretamente
+import ModalEdicaoEstoque from '../../components/Modais/Estoque/ModalEdicao';
 import ModalNovoEstoque from '../../components/Modais/Estoque/ModalNovo';
 import apiEstoque from '../../services/apiCliente'; // Importe a API correta para manipulação de Estoque
 
-// Defina o elemento de aplicação para react-modal
+// Definir o elemento de aplicação para react-modal
 Modal.setAppElement('#root');
 
 const Estoque = () => {
@@ -71,23 +71,25 @@ const Estoque = () => {
       const existingItem = itensEstoque.find(item => item.produtoID === formData.produtoID);
       if (existingItem) {
         alert('O produto já possui um estoque cadastrado. Redirecionando para a página de edição.');
-        openEdicaoModal(existingItem);
-      } else {
-        // Criação de novo item de estoque
-        const response = await apiEstoque.post('/Estoque', formData);
-        console.log('Novo item de estoque criado:', response.data);
-        
-        // Atualizar quantidade na tabela de produtos
-        const produtoResponse = await apiEstoque.put(`/Produto/${formData.produtoID}`, { quantidade: formData.quantidade });
-        console.log('Quantidade do produto atualizada:', produtoResponse.data);
-  
-        fetchItensEstoque(); // Atualiza lista de itens de estoque após salvar
-        closeModal();
+        openEdicaoModal(existingItem); // Abrir modal de edição com os dados do item existente
+        return; // Interrompe a execução após abrir o modal de edição
       }
+  
+      // Se não houver item existente, cria um novo item de estoque
+      const response = await apiEstoque.post('/Estoque', formData);
+      console.log('Novo item de estoque criado:', response.data);
+      
+      // Atualizar quantidade na tabela de produtos
+      const produtoResponse = await apiEstoque.put(`/Produto/${formData.produtoID}`, { quantidade: formData.quantidade });
+      console.log('Quantidade do produto atualizada:', produtoResponse.data);
+      fetchItensEstoque(); // Atualiza lista de itens de estoque após salvar
+      closeModal();
+      alert('Novo item de inventário criado com sucesso!');
     } catch (error) {
       console.error('Erro ao salvar item de estoque:', error);
     }
   };
+  
   
   const handleUpdate = async (formData) => {
     try {
