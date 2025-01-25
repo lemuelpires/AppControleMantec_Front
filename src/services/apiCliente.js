e73674bf85ce192bcdb94cf418bdb0f6712a8167
lemuelpires <107230579+lemuelpires@users.noreply.github.com>
2025-01-25 17:04:13 -0300

    atualização endereço api

--- src/services/apiCliente.js
+++ src/services/apiCliente.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiCliente = axios.create({
-  baseURL: 'https://www.portalmantec.com.br:5002/api',
+  baseURL: 'https://www.portalmantec.com.br:5004/api',
 });
 
 export default apiCliente;
