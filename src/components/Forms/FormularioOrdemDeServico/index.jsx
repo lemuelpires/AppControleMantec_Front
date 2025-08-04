a173a7b620b093e44e4517349291f1eb4704c9da
lemuelpires <107230579+lemuelpires@users.noreply.github.com>
2025-08-04 00:20:30 -0300

    melhoria no relatorio de ordem de serviço

--- src/components/Forms/FormularioOrdemDeServico/index.jsx
+++ src/components/Forms/FormularioOrdemDeServico/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FormGroup, Label, Input, Button, Espacamento, Form, Container, EspacamentoButton, EspacamentoContainer} from './style';
+import { FormGroup, Label, Input, Button, Espacamento, Form, Container, EspacamentoButton, EspacamentoContainer } from './style';
 import Select from 'react-select';
 import { height } from '@fortawesome/free-solid-svg-icons/fa0';
 
@@ -21,10 +21,7 @@ const FormularioOrdemDeServico = ({
     dataEntrada: '',
     dataConclusao: '',
     status: '',
-    informacaoProduto: '',
-    problemaAparente: '',
-    diagnosticoServico: '',
-    garantia: '',
+    observacoes: '',
     ...initialValues,
   });
 
@@ -46,50 +43,45 @@ const FormularioOrdemDeServico = ({
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    const observacoes = `
-      Informação do Produto: ${formData.informacaoProduto}
-      Problema aparente: ${formData.problemaAparente}
-      Diagnóstico e serviço a ser executado: ${formData.diagnosticoServico}
-      Garantia: ${formData.garantia}
-    `;
+    const observacoes = `${formData.observacoes || ''}`;
     onSubmit({ ...formData, observacoes, ativo: true });
   };
 
   const customSelectStyles = {
-  control: (provided, state) => ({
-    ...provided,
-    backgroundColor: 'white',
-    borderColor: state.isFocused ? '#016b19' : '#ccc',
-    boxShadow: state.isFocused ? '0 0 0 1px #016b19' : 'none',
-    height: '28px',         // Altura do select
-    minHeight: '28px',
-    fontSize: '12px',
-    backgroundColor: '#414040ff',
-    color: '#d9d9d9',
-  }),
-  option: (provided, state) => ({
-    ...provided,
-    backgroundColor: state.isFocused ? '#e5f4ea' : 'white',
-    color: 'black',
-    fontSize: '12px',
-  }),
-  menu: (provided) => ({
-    ...provided,
-    zIndex: 10,
-  }),
-  singleValue: (provided) => ({
-    ...provided,
-    color: '#d9d9d9',
-  }),
+    control: (provided, state) => ({
+      ...provided,
+      backgroundColor: 'white',
+      borderColor: state.isFocused ? '#016b19' : '#ccc',
+      boxShadow: state.isFocused ? '0 0 0 1px #016b19' : 'none',
+      height: '28px',         // Altura do select
+      minHeight: '28px',
+      fontSize: '12px',
+      backgroundColor: '#414040ff',
+      color: '#d9d9d9',
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isFocused ? '#e5f4ea' : 'white',
+      color: 'black',
+      fontSize: '12px',
+    }),
+    menu: (provided) => ({
+      ...provided,
+      zIndex: 10,
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: '#d9d9d9',
+    }),
 
-  dropdownIndicator: (provided, state) => ({
-    ...provided,
-    color: '#d9d9d9',
-    transform: state.selectProps.menuIsOpen ? 'rotate(180deg)' : null,
-    transition: 'transform 0.2s ease',
-    padding: '0px',
-  }),
-};
+    dropdownIndicator: (provided, state) => ({
+      ...provided,
+      color: '#d9d9d9',
+      transform: state.selectProps.menuIsOpen ? 'rotate(180deg)' : null,
+      transition: 'transform 0.2s ease',
+      padding: '0px',
+    }),
+  };
 
   return (
     <Container>
@@ -145,6 +137,39 @@ const FormularioOrdemDeServico = ({
                 styles={customSelectStyles}
               />
             </FormGroup>
+          </Espacamento>
+          <Espacamento>
+            <FormGroup>
+              <Label htmlFor="status">Status</Label>
+              <Select
+                id="status"
+                name="status"
+                options={[
+                  { value: 'Não iniciado', label: 'Não iniciado' },
+                  { value: 'Em andamento', label: 'Em andamento' },
+                  { value: 'Concluído', label: 'Concluído' },
+                  { value: 'Cancelado', label: 'Cancelado' },
+                ]}
+                value={
+                  formData.status
+                    ? { value: formData.status, label: formData.status }
+                    : null
+                }
+                onChange={(selectedOption) => handleSelectChange(selectedOption, 'status')}
+                placeholder="Selecione o status"
+                styles={customSelectStyles}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="observacoes">Diagnóstico e serviço a ser executado</Label>
+              <Input
+                type="text"
+                id="observacoes"
+                name="observacoes"
+                value={formData.observacoes || ''}
+                onChange={handleChange}
+              />
+            </FormGroup>
             <FormGroup>
               <Label htmlFor="dataEntrada">Data de Entrada</Label>
               <Input
@@ -166,62 +191,10 @@ const FormularioOrdemDeServico = ({
               />
             </FormGroup>
           </Espacamento>
-          <Espacamento>
-            <FormGroup>
-              <Label htmlFor="status">Status</Label>
-              <Input
-                type="text"
-                id="status"
-                name="status"
-                value={formData.status || ''}
-                onChange={handleChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label htmlFor="informacaoProduto">Informação do Produto</Label>
-              <Input
-                type="text"
-                id="informacaoProduto"
-                name="informacaoProduto"
-                value={formData.informacaoProduto || ''}
-                onChange={handleChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label htmlFor="problemaAparente">Problema Aparente</Label>
-              <Input
-                type="text"
-                id="problemaAparente"
-                name="problemaAparente"
-                value={formData.problemaAparente || ''}
-                onChange={handleChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label htmlFor="diagnosticoServico">Diagnóstico e serviço a ser executado</Label>
-              <Input
-                type="text"
-                id="diagnosticoServico"
-                name="diagnosticoServico"
-                value={formData.diagnosticoServico || ''}
-                onChange={handleChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label htmlFor="garantia">Garantia</Label>
-              <Input
-                type="text"
-                id="garantia"
-                name="garantia"
-                value={formData.garantia || ''}
-                onChange={handleChange}
-              />
-            </FormGroup>
-          </Espacamento>
         </EspacamentoContainer>
         <EspacamentoButton>
-          <Button style={{ backgroundColor: '#016b19ff', color: 'white', padding: '5px 10px', border: 'none', borderRadius: '4px', cursor: 'pointer', marginRight: '10px' }} type="submit">Salvar</Button>
-          <Button style={{ backgroundColor: '#6d0b02ff', color: 'white', padding: '5px 10px', border: 'none', borderRadius: '4px', cursor: 'pointer' }} type="button" onClick={onClose}>Cancelar</Button>
+          <Button style={{ backgroundColor: '#0f9d58', color: 'white', padding: '5px 10px', border: 'none', borderRadius: '4px', cursor: 'pointer', marginRight: '10px' }} type="submit">Salvar</Button>
+          <Button style={{ backgroundColor: '#e53935', color: 'white', padding: '5px 10px', border: 'none', borderRadius: '4px', cursor: 'pointer' }} type="button" onClick={onClose}>Cancelar</Button>
         </EspacamentoButton>
       </Form>
     </Container>
