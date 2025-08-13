b76780cdf20daa266334a68855f5d33a04ebd8a3
lemuelpires <107230579+lemuelpires@users.noreply.github.com>
2025-08-13 00:17:30 -0300

    correção dao endereço do endpoint

--- src/services/apiCliente.js
+++ src/services/apiCliente.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiCliente = axios.create({
-  /*baseURL: 'https://www.portalmantec.com.br/mantec/api',*/
-    baseURL: 'http://www.portalmantec.com.br:5010/api',
+  baseURL: 'https://www.portalmantec.com.br/mantec/api',
+  /*baseURL: 'http://www.portalmantec.com.br:5010/api',*/
   /*baseURL: 'http://localhost:5003/api',*/
 });
 
