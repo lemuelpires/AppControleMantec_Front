3c544dd295193f3290f3fcc9d8ad240ffabeb9e0
lemuelpires <107230579+lemuelpires@users.noreply.github.com>
2025-08-13 09:46:12 -0300

    correção da api

--- src/services/apiCliente.js
+++ src/services/apiCliente.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const apiCliente = axios.create({
-  /*baseURL: 'https://www.portalmantec.com.br/mantec/api',*/
   baseURL: 'http://www.portalmantec.com.br:5010/api',
-  /*baseURL: 'http://localhost:5003/api',*/
 });
 
 
